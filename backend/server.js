0f79dbc4708bf772ed661119afd971b8af7cbc56
sharmaprakhar475 <sharmaprakhar475@gmail.com>
2025-11-15 14:11:17 +0530

    bug fixed path

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 connectDB();
 const PORT = process.env.PORT || 5000;
