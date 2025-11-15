c09cd5c1a8e9ed193d2e842ba6f917370cd332ca
sharmaprakhar475 <sharmaprakhar475@gmail.com>
2025-11-15 14:07:22 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,21 @@ const cors = require("cors");
 
 const app = express();
 connectDB();
-
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/courses", require("./routes/courseRoutes"));
 app.use("/api/payment", require("./routes/payment"));
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  // If the route is NOT an API route → serve React
+  if (!req.url.startsWith("/api")) {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  }
+});
+
+app.listen(PORT, () => console.log("Server running on port 5000"));
